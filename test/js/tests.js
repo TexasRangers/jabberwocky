6b477026015f0c429a5ef028ec3c2b51692255ec
Lukasz Wiechec <coquelicot408@gmail.com>
2012-12-06 00:21:00 +0100

    Update test/js/tests.js
    dodany test na szerokość tabicy 'pola[]'

--- test/js/tests.js
+++ test/js/tests.js
@@ -4,3 +4,7 @@ test( "test długości tablicy z obrazkami", function() {
     var obrazki = inicjuj_obrazki();
     equal( obrazki.length, 15, "zła długość tablicy, powinno być 15 " );
 });
+
+test( "czy inicjalizacja zmiennej 'pola[]' przebiegła poprawnie", function() {
+    equal( pole.length, 25, "rozmiar zmiennej 'pola[]' nieprawidłowy" );
+})
